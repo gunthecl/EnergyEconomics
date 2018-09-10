2dfb99dc6745798c0af2e90f4b86410e9e053e05
gunthecl <33225856+gunthecl@users.noreply.github.com>
2018-09-10 18:46:16 +0200

    changed clustering

--- SeminarPaper/Code/R/Test_weather.R
+++ SeminarPaper/Code/R/Test_weather.R
@@ -26,7 +26,7 @@ lapply(neededPackages, function(x) suppressPackageStartupMessages(
 ################################################################################
 
 ## Change path to working directory
-wd.path = "/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Code/R/"
+wd.path = "/Users/Lenovo/Documents/Github/EnergyEconomics/SeminarPaper/Code/R/"
 
 
 # Read in wind and pv data set
@@ -82,16 +82,18 @@ load.vec         <- dat.load[rep(seq_len(nrow(dat.load)), each=nrow(dat.original
 
 dat.original     <- cbind(dat.original, load.vec)
 
+rm(dat.load, dat.load.2014, var.vec, var.vec.load, years.sequence,
+   dat.res, load.vec, hours.year, leap.days.vec, leap.hours,
+   neededPackages, allPackages)
+
 # Normalize with maximum value
 dat <-  as.data.frame(apply(dat.original, 2, function(x){
   
              ( x - max(x)) / (max(x) - min(x))
                                                 }))
 
-#dat <-  as.data.frame(apply(dat.original, 2, function(x){
-  
+#dat2 <-  as.data.frame(apply(dat.original, 2, function(x){
 #  ( x - mean(x)) / sd(x)
-  
 #}))
 
 # Keep only every third hour
@@ -102,9 +104,11 @@ dat <-  as.data.frame(apply(dat.original, 2, function(x){
 dat.new  <- list()
 
 # Drop observations to deal with full weaks
-no.drop <- nrow(dat.original)-floor(nrow(dat.original)/7/24)*7*24
-dat     <- dat[-c((nrow(dat)-no.drop+1):nrow(dat)),]
+no.drop      <- nrow(dat.original)-floor(nrow(dat.original)/7/24)*7*24
+dat          <- dat[-c((nrow(dat)-no.drop+1):nrow(dat)),]
+dat.unscaled <- dat.original[-c((nrow(dat.original)-no.drop+1):nrow(dat.original)),]
 
+# Extend data set to wide format (each hour as variable)
 for (i in 1:(nrow(dat)/(24))){
   
   j       <- i*24 - 23
@@ -121,6 +125,23 @@ for (i in 1:(nrow(dat)/(24))){
 
 dat <- data.frame(list.rbind(dat.new))
 
+# Repeat for unscaled data
+for (i in 1:(nrow(dat.unscaled)/(24))){
+  
+  j       <- i*24 - 23
+  
+  dat.day   <- dat.unscaled[j:(j+23),]
+  data_long <- melt(dat.day)
+  data_wide <- t(data_long$value)
+  colnames(data_wide) <- data_long$variable
+  
+  dat.new[[i]] <- data_wide
+  
+}
+
+
+dat.unscaled <- data.frame(list.rbind(dat.new))
+
 
 # Use euclidean distance
 Dis.ecl <- dist(dat, method = "euclidean")
@@ -134,7 +155,7 @@ dendrogram <- as.dendrogram(clus)
 plot(dendrogram, ylab = "Height", leaflab = "none")
 
 # Determine the number of clusters
-no.cl    = 40
+no.cl    = 60
 clusters = cutree(clus, k = no.cl) 
 rect.hclust(clus, k= no.cl, border="red") 
 
@@ -150,9 +171,9 @@ final.table <- table(cluster.k$cluster, clusters) # relatively stable
 final.memb  <- cluster.k$cluster 
 
 # Find centroids and medoids of clusters
-centroid.vec  <- clust.centroid(dataframe = dat, clusters.IND = cluster.k$cluster)
+centroid.vec  <- clust.centroid(dataframe = dat.unscaled, clusters.IND = cluster.k$cluster)
 medoid.IND    <- clust.medoid(clusters.IND = cluster.k$cluster, distancematrix = as.matrix(Dis.ecl))
-medoid.vec    <- dat[medoid.IND,]
+medoid.vec    <- dat.unscaled[medoid.IND,]
     
 ################################################################################
 # Check suitability of cluster solution
@@ -172,8 +193,7 @@ days.repl    <- for (i in 1:length(cluster.k$size)){
 test.GB      <- data.frame(list.rbind(days.list))
 all.hours.GB <- unlist(test.GB)
 org.GB       <- dat.original[-c((nrow(dat.original)-no.drop+1):nrow(dat.original)),]
-org.GB       <- sort((  (org.GB$GB_load_entsoe_power_statistics - max(org.GB$GB_load_entsoe_power_statistics))/
-                       (max(org.GB$GB_load_entsoe_power_statistics)- min(org.GB$GB_load_entsoe_power_statistics))), decreasing = TRUE)
+org.GB       <- sort(org.GB$GB_load_entsoe_power_statistics, decreasing = TRUE)
 
 par(mar = rep(2, 4))
 plot(sort(all.hours.GB, decreasing = TRUE),type="l",col="red")
@@ -183,3 +203,162 @@ length(org.GB) == length(all.hours.GB)
 
 
 ######################################################################
+## Compare load duration, wind and pv factor curves for Germany
+
+# Find columns of German load data
+col.load.GER     <- grepl("DE_load", colnames(medoid.vec))
+GER.load         <- medoid.vec[,col.load.GER]
+col.load.GER.org <- grepl("DE_load", colnames(dat.original))
+
+# Mutiply each cluster with its size
+days.list   <- list()
+days.repl   <- for (i in 1:length(cluster.k$size)){
+  
+  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  
+  days.list[[i]] <- x
+  
+}
+
+GER.load.repl <- data.frame(list.rbind(days.list))
+GER.load.repl <- unlist(GER.load.repl)
+GER.load.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.load.GER.org]
+GER.load.org  <- sort(GER.load.org, decreasing = TRUE)
+
+par(mar = rep(2, 4))
+plot(sort(GER.load.repl, decreasing = TRUE),type="l",col="red")
+lines(GER.load.org, col="green")
+
+length(GER.load.org) == length(GER.load.repl)
+
+# Find columns of German pv data
+col.pv.GER     <- grepl("DE_pv", colnames(medoid.vec))
+GER.load         <- medoid.vec[,col.pv.GER]
+col.pv.GER.org <- grepl("DE_pv", colnames(dat.original))
+
+# Mutiply each cluster with its size
+days.list   <- list()
+days.repl   <- for (i in 1:length(cluster.k$size)){
+  
+  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  
+  days.list[[i]] <- x
+  
+}
+
+GER.pv.repl <- data.frame(list.rbind(days.list))
+GER.pv.repl <- unlist(GER.pv.repl)
+GER.pv.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.pv.GER.org]
+GER.pv.org  <- sort(GER.pv.org, decreasing = TRUE)
+
+par(mar = rep(2, 4))
+plot(sort(GER.pv.repl, decreasing = TRUE),type="l",col="red")
+lines(GER.pv.org, col="green")
+
+length(GER.pv.org) == length(GER.pv.repl)
+
+# Find columns of German onshore wind data
+col.wind_on.GER     <- grepl("DE_wind_on", colnames(medoid.vec))
+GER.load            <- medoid.vec[,col.wind_on.GER]
+col.wind_on.GER.org <- grepl("DE_wind_on", colnames(dat.original))
+
+# Mutiply each cluster with its size
+days.list   <- list()
+days.repl   <- for (i in 1:length(cluster.k$size)){
+  
+  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  
+  days.list[[i]] <- x
+  
+}
+
+GER.wind_on.repl <- data.frame(list.rbind(days.list))
+GER.wind_on.repl <- unlist(GER.wind_on.repl)
+GER.wind_on.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.wind_on.GER.org]
+GER.wind_on.org  <- sort(GER.wind_on.org, decreasing = TRUE)
+
+par(mar = rep(2, 4))
+plot(sort(GER.wind_on.repl, decreasing = TRUE),type="l",col="red")
+lines(GER.wind_on.org, col="green")
+
+length(GER.wind_on.org) == length(GER.wind_on.repl)
+
+# Find columns of German offshore wind data
+col.wind_off.GER     <- grepl("DE_wind_off", colnames(medoid.vec))
+GER.load         <- medoid.vec[,col.wind_off.GER]
+col.wind_off.GER.org <- grepl("DE_wind_off", colnames(dat.original))
+
+# Mutiply each cluster with its size
+days.list   <- list()
+days.repl   <- for (i in 1:length(cluster.k$size)){
+  
+  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  
+  days.list[[i]] <- x
+  
+}
+
+GER.wind_off.repl <- data.frame(list.rbind(days.list))
+GER.wind_off.repl <- unlist(GER.wind_off.repl)
+GER.wind_off.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.wind_off.GER.org]
+GER.wind_off.org  <- sort(GER.wind_off.org, decreasing = TRUE)
+
+par(mar = rep(2, 4))
+plot(sort(GER.wind_off.repl, decreasing = TRUE),type="l",col="red")
+lines(GER.wind_off.org, col="green")
+
+length(GER.wind_off.org) == length(GER.wind_off.repl)
+
+################################################################################
+# Reproduce 2014 weather year with cluster medoids
+
+days.2014       <- {365*28+1}:{365*29}
+hours.2014      <- {365*29*24+1}:{365*30*24}
+cluster.2014    <- cluster.k$cluster[days.2014]
+
+dat.2014.medoid.raw  <- medoid.vec[cluster.2014,]
+dat.2014.medoid.list <- list()
+# Merge hours back into one vector
+for (i in 1:16){
+  
+    vec.hours <- {i*24-23}:{i*24}
+    col.vec   <-  dat.2014.medoid.raw[,vec.hours]
+    day       <- list()
+    
+    for (j in 1:nrow(col.vec)){
+      
+      day[[j]] <- as.numeric(col.vec[j,])
+      
+      
+    }
+    
+    dat.2014.medoid.list[[i]] <- unlist(t(day))
+  
+  
+}
+
+# Finish replicated and original 2014 data set
+dat.2014.medoid           <- data.frame(t(list.rbind(dat.2014.medoid.list)))
+colnames(dat.2014.medoid) <- colnames(dat.original)
+dat.2014.org              <- dat.original[hours.2014,]
+
+# Rescale replicated data set
+g = apply(dat.2014.org, 2, mean)
+t = apply(dat.2014.medoid, 2, mean)
+n = apply(dat.2014.org, 2, sd)
+m = apply(dat.2014.medoid, 2, sd)
+gamma = (t-g)
+delta = m/n
+f = as.data.frame(scale(dat.2014.medoid, center = gamma, scale = delta))
+
+rm(dat.2014.medoid.raw, dat.2014.medoid.list)
+
+# Compare both data sets
+par(mar = rep(2, 4))
+plot(sort(f$FR_load_entsoe_power_statistics, decreasing = TRUE),type="l",col="red")
+lines(sort(dat.2014.org$FR_load_entsoe_power_statistics, decreasing = TRUE), col="green")
+
+summary(f$FR_load_entsoe_power_statistics)
+summary(dat.2014.medoid$FR_load_entsoe_power_statistics)
+summary(dat.original$FR_load_entsoe_power_statistics)
+
