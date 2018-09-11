181864dfe116460402674698af6ae7dbcc161033
Claudia <claudia-jena@gmx.de>
2018-09-11 21:41:16 +0200

    Finished clustering

--- SeminarPaper/Code/R/Test_weather.R
+++ SeminarPaper/Code/R/Test_weather.R
@@ -26,13 +26,13 @@ lapply(neededPackages, function(x) suppressPackageStartupMessages(
 ################################################################################
 
 ## Change path to working directory
-wd.path = "/Users/Lenovo/Documents/Github/EnergyEconomics/SeminarPaper/Code/R/"
+wd.path = "/Users/claudiaguenther/documents/EnergyEconomics/SeminarPaper/Code/R/"
 
 
 # Read in wind and pv data set
-dat.res            <- read.csv(paste0(wd.path,"ninja_pv_wind_profiles_1985-2016.csv"), 
+dat.res            <- read.csv(paste0(wd.path,"ninja_pv_wind_profiles_1985-2016_final.csv"), 
                           stringsAsFactors = FALSE)
-dat.load.2014      <- read.csv(paste0(wd.path,"time_series_60min_load.csv"), 
+dat.load.2015      <- read.csv(paste0(wd.path,"time_series_load_2015_final.csv"), 
                           stringsAsFactors = FALSE)
 
 # Source needed functions
@@ -40,19 +40,54 @@ source("HelperFunctions.R")
 
 ################################################################################
 
+# Use weighted average for Spain & Portugal, Luxembourg & Netherlands & Belgium
+
+dat.res$BNL_pv_national_current   <- (dat.res$BE_pv_national_current*0.426 
+                                    + dat.res$LU_pv_national_current*0.034
+                                    + dat.res$NL_pv_national_current*0.54)
+
+dat.res$IBE_pv_national_current   <- (dat.res$ES_pv_national_current*0.845 
+                                   + dat.res$PT_pv_national_current*0.155)
+
+dat.res$BNL_wind_onshore_current  <- (dat.res$BE_wind_onshore_current*0.426 
+                                     + dat.res$LU_wind_national_current*0.034
+                                     + dat.res$NL_wind_onshore_current*0.54)
+
+dat.res$IBE_wind_onshore_current  <-  (dat.res$ES_wind_national_current*0.845 
+                                     + dat.res$PT_wind_national_current*0.155)
+
+dat.res$BNL_wind_offshore_current <- (dat.res$BE_wind_offshore_current*0.441 
+                                    + dat.res$NL_wind_offshore_current*0.559)
+
+# Sum load for Spain & Portugal, Luxembourg & Netherlands & Belgium
+
+dat.load.2015$BNL_load_entsoe_power_statistics <-  ( dat.load.2015$BE_load_entsoe_power_statistics
+                                                   + dat.load.2015$LU_load_entsoe_power_statistics
+                                                   + dat.load.2015$NL_load_entsoe_power_statistics)
+    
+dat.load.2015$IBE_load_entsoe_power_statistics <-  ( dat.load.2015$ES_load_entsoe_power_statistics
+                                                     + dat.load.2015$PT_load_entsoe_power_statistics)
+
+
 # Select columns 
-var.vec     <- c("DE_pv_national_current", "DE_wind_onshore_current", "DE_wind_offshore_current",
-           "DK_pv_national_current", "DK_wind_onshore_current", "DK_wind_offshore_current",
-          # "ES_pv_national_current", "ES_wind_national_current",
-           "FR_pv_national_current", "FR_wind_onshore_current", "FR_wind_offshore_current",
-           "GB_pv_national_current", "GB_wind_onshore_current", "GB_wind_offshore_current")
-var.vec.load <- c("DE_load_entsoe_power_statistics", 
+var.vec     <- c("BNL_pv_national_current", "BNL_wind_onshore_current", "BNL_wind_offshore_current",
+                 "DE_pv_national_current", "DE_wind_onshore_current", "DE_wind_offshore_current",
+                 "DK_pv_national_current", "DK_wind_onshore_current", "DK_wind_offshore_current",
+                  "ES_pv_national_current", "ES_wind_national_current",
+                  "FR_pv_national_current", "FR_wind_onshore_current", "FR_wind_offshore_current",
+                  "GB_pv_national_current", "GB_wind_onshore_current", "GB_wind_offshore_current",
+                  "PT_pv_national_current", "PT_wind_national_current")
+
+var.vec.load <- c("BNL_load_entsoe_power_statistics",
+                  "DE_load_entsoe_power_statistics", 
                   "DK_load_entsoe_power_statistics",
-#                  "ES_load_entsoe_power_statistics",
+                  "IBE_load_entsoe_power_statistics",
                   "FR_load_entsoe_power_statistics",
                   "GB_load_entsoe_power_statistics")
+
 dat.original <- dat.res[,var.vec]
-dat.load     <- dat.load.2014[,var.vec.load]
+
+dat.load     <- dat.load.2015[,var.vec.load]
 
 # Drop leap year days
 leap.hours      <- c((59*24+1):(59*24+24))
@@ -82,7 +117,7 @@ load.vec         <- dat.load[rep(seq_len(nrow(dat.load)), each=nrow(dat.original
 
 dat.original     <- cbind(dat.original, load.vec)
 
-rm(dat.load, dat.load.2014, var.vec, var.vec.load, years.sequence,
+rm(dat.load, dat.load.2015, var.vec, var.vec.load, years.sequence,
    dat.res, load.vec, hours.year, leap.days.vec, leap.hours,
    neededPackages, allPackages)
 
@@ -101,12 +136,13 @@ dat <-  as.data.frame(apply(dat.original, 2, function(x){
 
 # Reshape data (each hour becomes a variable)
 #dat$hour <- rep(paste0("h", 1:24), nrow(dat)/24)
-dat.new  <- list()
+dat.new      <- list()
+dat.unscaled <- dat.original
 
 # Drop observations to deal with full weaks
-no.drop      <- nrow(dat.original)-floor(nrow(dat.original)/7/24)*7*24
-dat          <- dat[-c((nrow(dat)-no.drop+1):nrow(dat)),]
-dat.unscaled <- dat.original[-c((nrow(dat.original)-no.drop+1):nrow(dat.original)),]
+#no.drop      <- nrow(dat.original)-floor(nrow(dat.original)/7/24)*7*24
+#dat          <- dat[-c((nrow(dat)-no.drop+1):nrow(dat)),]
+#dat.unscaled <- dat.original[-c((nrow(dat.original)-no.drop+1):nrow(dat.original)),]
 
 # Extend data set to wide format (each hour as variable)
 for (i in 1:(nrow(dat)/(24))){
@@ -148,58 +184,27 @@ Dis.ecl <- dist(dat, method = "euclidean")
 
 # Hierachcal cluster analysis based on Ward & euclidean distance
 clus <- hclust(Dis.ecl, method = "ward.D2")
-par(mar = rep(2, 4))
-plot(clus)
-plot(clus, hang = -1, cex = 0.6)
-dendrogram <- as.dendrogram(clus)
-plot(dendrogram, ylab = "Height", leaflab = "none")
+#par(mar = rep(2, 4))
+#plot(clus)
+#plot(clus, hang = -1, cex = 0.6)
+#dendrogram <- as.dendrogram(clus)
+#plot(dendrogram, ylab = "Height", leaflab = "none")
 
 # Determine the number of clusters
-no.cl    = 60
+no.cl    = 50
 clusters = cutree(clus, k = no.cl) 
 rect.hclust(clus, k= no.cl, border="red") 
 
-# Get centroids: Use for k mean initialization
-centroids = clust.centroid(dataframe = dat, clusters.IND = clusters)
-
-# Run k means on extracted centroids
-cluster.k          <-  kmeans(dat, centers = centroids)
-cluster.center     <-  cluster.k$centers
-
-# Oberve change of cluster membership
-final.table <- table(cluster.k$cluster, clusters) # relatively stable
-final.memb  <- cluster.k$cluster 
-
 # Find centroids and medoids of clusters
-centroid.vec  <- clust.centroid(dataframe = dat.unscaled, clusters.IND = cluster.k$cluster)
-medoid.IND    <- clust.medoid(clusters.IND = cluster.k$cluster, distancematrix = as.matrix(Dis.ecl))
+centroid.vec  <- clust.centroid(dataframe = dat.unscaled, clusters.IND = clusters)
+medoid.IND    <- clust.medoid(clusters.IND = clusters, distancematrix = as.matrix(Dis.ecl))
 medoid.vec    <- dat.unscaled[medoid.IND,]
     
-################################################################################
-# Check suitability of cluster solution
+# Determine cluster sizes
+cluster.size  <- table(clusters)
 
-# Compare original LDC and fitted (here GB)
-
-test.GB.load <- medoid.vec[,361:384]
-days.list    <- list()
-days.repl    <- for (i in 1:length(cluster.k$size)){
-  
-              x <- as.data.frame(test.GB.load[rep(i, cluster.k$size[i]),])  
-            
-              days.list[[i]] <- x
-              
-}
-
-test.GB      <- data.frame(list.rbind(days.list))
-all.hours.GB <- unlist(test.GB)
-org.GB       <- dat.original[-c((nrow(dat.original)-no.drop+1):nrow(dat.original)),]
-org.GB       <- sort(org.GB$GB_load_entsoe_power_statistics, decreasing = TRUE)
-
-par(mar = rep(2, 4))
-plot(sort(all.hours.GB, decreasing = TRUE),type="l",col="red")
-lines(org.GB, col="green")
-
-length(org.GB) == length(all.hours.GB)
+######################################################################
+# Rescale replicated data set
 
 
 ######################################################################
@@ -212,24 +217,40 @@ col.load.GER.org <- grepl("DE_load", colnames(dat.original))
 
 # Mutiply each cluster with its size
 days.list   <- list()
-days.repl   <- for (i in 1:length(cluster.k$size)){
+days.repl   <- for (i in 1:length(cluster.size)){
   
-  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  x <- as.data.frame(GER.load[rep(i, cluster.size[i]),])  
   
   days.list[[i]] <- x
   
 }
 
 GER.load.repl <- data.frame(list.rbind(days.list))
-GER.load.repl <- unlist(GER.load.repl)
-GER.load.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.load.GER.org]
+GER.load.repl <- sort(unlist(GER.load.repl), decreasing = TRUE)
+GER.load.org  <- dat.original[,col.load.GER.org]
 GER.load.org  <- sort(GER.load.org, decreasing = TRUE)
 
-par(mar = rep(2, 4))
-plot(sort(GER.load.repl, decreasing = TRUE),type="l",col="red")
-lines(GER.load.org, col="green")
+GER.load.both <- as.data.frame(cbind(GER.load.org, GER.load.repl))
+GER.load.both$hour <- 1:nrow(GER.load.both)
+ggplot(GER.load.both, aes(x = hour)) + 
+    geom_line(aes(y = GER.load.org, colour = "Original LDC" )) + 
+    geom_line(aes(y = GER.load.repl, colour = "Replicated LDC")) + 
+    scale_colour_manual("", 
+                        breaks = c("Original LDC", "Replicated LDC"),
+                        values = c("grey", "brown")) +
+    ylab(label="Load in MWh") + 
+    xlab("Hours") + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "grey")) + theme(
+              plot.margin=unit(c(1,1,1,2), "cm"),
+              panel.grid = element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.text.x=element_blank(),
+              panel.background = element_blank(),
+              legend.key=element_blank())
 
-length(GER.load.org) == length(GER.load.repl)
+rm(GER.load.both, GER.load.org, GER.load.repl,
+   col.load.GER, col.load.GER.org)
 
 # Find columns of German pv data
 col.pv.GER     <- grepl("DE_pv", colnames(medoid.vec))
@@ -238,24 +259,42 @@ col.pv.GER.org <- grepl("DE_pv", colnames(dat.original))
 
 # Mutiply each cluster with its size
 days.list   <- list()
-days.repl   <- for (i in 1:length(cluster.k$size)){
+days.repl   <- for (i in 1:length(cluster.size)){
   
-  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  x <- as.data.frame(GER.load[rep(i, cluster.size[i]),])  
   
   days.list[[i]] <- x
   
 }
 
 GER.pv.repl <- data.frame(list.rbind(days.list))
-GER.pv.repl <- unlist(GER.pv.repl)
-GER.pv.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.pv.GER.org]
+GER.pv.repl <- sort(unlist(GER.pv.repl), decreasing = TRUE)
+GER.pv.org  <- dat.original[,col.pv.GER.org]
 GER.pv.org  <- sort(GER.pv.org, decreasing = TRUE)
 
-par(mar = rep(2, 4))
-plot(sort(GER.pv.repl, decreasing = TRUE),type="l",col="red")
-lines(GER.pv.org, col="green")
 
-length(GER.pv.org) == length(GER.pv.repl)
+GER.pv.both <- as.data.frame(cbind(GER.pv.org, GER.pv.repl))
+GER.pv.both$hour <- 1:nrow(GER.pv.both)
+ggplot(GER.pv.both, aes(x = hour)) + 
+    geom_line(aes(y = GER.pv.org, colour = "Original pv series" )) + 
+    geom_line(aes(y = GER.pv.repl, colour = "Replicated pv series")) + 
+    scale_colour_manual("", 
+                        breaks = c("Original pv series", "Replicated pv series"),
+                        values = c("grey", "orange")) +
+    ylab(label="Load in MWh") + 
+    xlab("Hours") + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "grey")) + theme(
+              plot.margin=unit(c(1,1,1,2), "cm"),
+              panel.grid = element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.text.x=element_blank(),
+              panel.background = element_blank(),
+              legend.key=element_blank())
+
+
+rm(GER.pv.both, GER.pv.org, GER.pv.repl,
+   col.pv.GER, col.pv.GER.org)
 
 # Find columns of German onshore wind data
 col.wind_on.GER     <- grepl("DE_wind_on", colnames(medoid.vec))
@@ -264,9 +303,9 @@ col.wind_on.GER.org <- grepl("DE_wind_on", colnames(dat.original))
 
 # Mutiply each cluster with its size
 days.list   <- list()
-days.repl   <- for (i in 1:length(cluster.k$size)){
+days.repl   <- for (i in 1:length(cluster.size)){
   
-  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  x <- as.data.frame(GER.load[rep(i, cluster.size[i]),])  
   
   days.list[[i]] <- x
   
@@ -274,7 +313,7 @@ days.repl   <- for (i in 1:length(cluster.k$size)){
 
 GER.wind_on.repl <- data.frame(list.rbind(days.list))
 GER.wind_on.repl <- unlist(GER.wind_on.repl)
-GER.wind_on.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.wind_on.GER.org]
+GER.wind_on.org  <- dat.original[,col.wind_on.GER.org]
 GER.wind_on.org  <- sort(GER.wind_on.org, decreasing = TRUE)
 
 par(mar = rep(2, 4))
@@ -290,9 +329,9 @@ col.wind_off.GER.org <- grepl("DE_wind_off", colnames(dat.original))
 
 # Mutiply each cluster with its size
 days.list   <- list()
-days.repl   <- for (i in 1:length(cluster.k$size)){
+days.repl   <- for (i in 1:length(cluster.size)){
   
-  x <- as.data.frame(GER.load[rep(i, cluster.k$size[i]),])  
+  x <- as.data.frame(GER.load[rep(i, cluster.size[i]),])  
   
   days.list[[i]] <- x
   
@@ -300,7 +339,7 @@ days.repl   <- for (i in 1:length(cluster.k$size)){
 
 GER.wind_off.repl <- data.frame(list.rbind(days.list))
 GER.wind_off.repl <- unlist(GER.wind_off.repl)
-GER.wind_off.org  <- dat.original[1:(nrow(dat.original)-no.drop),col.wind_off.GER.org]
+GER.wind_off.org  <- dat.original[,col.wind_off.GER.org]
 GER.wind_off.org  <- sort(GER.wind_off.org, decreasing = TRUE)
 
 par(mar = rep(2, 4))
@@ -312,14 +351,14 @@ length(GER.wind_off.org) == length(GER.wind_off.repl)
 ################################################################################
 # Reproduce 2014 weather year with cluster medoids
 
-days.2014       <- {365*28+1}:{365*29}
-hours.2014      <- {365*29*24+1}:{365*30*24}
-cluster.2014    <- cluster.k$cluster[days.2014]
+days.2014       <- {(365*29)+1}:{365*30}
+hours.2014      <- {(365*29*24)+1}:{365*30*24}
+cluster.2014    <- clusters[days.2014]
 
 dat.2014.medoid.raw  <- medoid.vec[cluster.2014,]
 dat.2014.medoid.list <- list()
 # Merge hours back into one vector
-for (i in 1:16){
+for (i in 1:25){
   
     vec.hours <- {i*24-23}:{i*24}
     col.vec   <-  dat.2014.medoid.raw[,vec.hours]
@@ -343,22 +382,77 @@ colnames(dat.2014.medoid) <- colnames(dat.original)
 dat.2014.org              <- dat.original[hours.2014,]
 
 # Rescale replicated data set
-g = apply(dat.2014.org, 2, mean)
-t = apply(dat.2014.medoid, 2, mean)
-n = apply(dat.2014.org, 2, sd)
-m = apply(dat.2014.medoid, 2, sd)
-gamma = (t-g)
-delta = m/n
-f = as.data.frame(scale(dat.2014.medoid, center = gamma, scale = delta))
+mean.org   = apply(dat.2014.org, 2, mean)
+mean.clust = apply(dat.2014.medoid, 2, mean)
+gamma      = (mean.clust-mean.org)
+dat.2014.medoid.scaled = as.data.frame(scale(dat.2014.medoid, center = gamma, scale = FALSE))
 
 rm(dat.2014.medoid.raw, dat.2014.medoid.list)
 
 # Compare both data sets
-par(mar = rep(2, 4))
-plot(sort(f$FR_load_entsoe_power_statistics, decreasing = TRUE),type="l",col="red")
-lines(sort(dat.2014.org$FR_load_entsoe_power_statistics, decreasing = TRUE), col="green")
+dat.compare  <- cbind(dat.2014.medoid.scaled, dat.2014.org)
+vec.names    <- colnames(dat.2014.org)
+colnames(dat.compare) <- c(paste0(vec.names, ".rep"), paste0(vec.names, ".org"))
+dat.compare$hour  <- 1:8760
 
-summary(f$FR_load_entsoe_power_statistics)
-summary(dat.2014.medoid$FR_load_entsoe_power_statistics)
-summary(dat.original$FR_load_entsoe_power_statistics)
+
+ggplot(dat.compare, aes(x = hour)) + 
+    geom_line(aes(y = sort(DE_load_entsoe_power_statistics.org, 
+                           decreasing = TRUE), colour = "Original LDC" )) + 
+    geom_line(aes(y = sort(DE_load_entsoe_power_statistics.rep, 
+                           decreasing = TRUE), colour = "Replicated LDC")) + 
+    scale_colour_manual("", 
+                        breaks = c("Original LDC", "Replicated LDC"),
+                        values = c("grey", "brown")) +
+    ylab(label="Load in MWh") + 
+    xlab("Hour") + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "grey")) + theme(
+              plot.margin=unit(c(1,1,1,2), "cm"),
+              panel.grid = element_blank(), 
+              panel.background = element_blank(),
+              legend.key=element_blank())
+
+
+ggplot(dat.compare, aes(x = hour)) + 
+    geom_line(aes(y = sort(DE_pv_national_current.org, 
+                           decreasing = TRUE), colour = "Original pv curve" )) + 
+    geom_line(aes(y = sort(DE_pv_national_current.rep, 
+                           decreasing = TRUE), colour = "Replicated pv curve")) + 
+    scale_colour_manual("", 
+                        breaks = c("Original pv curve", "Replicated pv curve"),
+                        values = c("grey", "orange")) +
+    ylab(label="Capacity factor") + 
+    xlab("Hour") + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "grey")) + theme(
+              plot.margin=unit(c(1,1,1,2), "cm"),
+              panel.grid = element_blank(), 
+              panel.background = element_blank(),
+              legend.key=element_blank())
+
+
+ggplot(dat.compare, aes(x = hour)) + 
+    geom_line(aes(y = sort(DE_wind_onshore_current.org, 
+                           decreasing = TRUE), colour = "Original onshore wind" )) + 
+    geom_line(aes(y = sort(DE_wind_onshore_current.rep, 
+                           decreasing = TRUE), colour = "Replicated onshore wind")) + 
+    geom_line(aes(y = sort(DE_wind_offshore_current.org, 
+                           decreasing = TRUE), colour = "Original offshore wind" )) + 
+    geom_line(aes(y = sort(DE_wind_offshore_current.rep, 
+                           decreasing = TRUE), colour = "Replicated offshore wind")) + 
+    scale_colour_manual("", 
+                        breaks = c("Original onshore wind", "Replicated onshore wind",
+                                   "Original offshore wind", "Replicated offshore wind" ),
+                        values = c("grey81",  "light blue","grey39","dark blue")) +
+    ylab(label="Capacity factor") + 
+    xlab("Hour") + 
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "grey")) + theme(
+              plot.margin=unit(c(1,1,1,2), "cm"),
+              panel.grid = element_blank(), 
+              panel.background = element_blank(),
+              legend.key=element_blank())
+
+
 
