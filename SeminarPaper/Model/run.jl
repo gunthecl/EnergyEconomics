7c6e8bac8bea747d651bf284cd9d8956d58a8575
Unknown <chgentsch@googlemail.com>
2018-09-27 13:00:50 +0200

    Add function for saving results, Fix model

--- SeminarPaper/Model/run.jl
+++ SeminarPaper/Model/run.jl
@@ -5,6 +5,9 @@ using NamedArrays
 using JuliaDB
 using DataStructures
 using RData
+using CSV
+using DataFrames
+using Dates
 using JuMP
 using Gurobi
 using Plots, StatPlots, UnicodePlots
@@ -12,37 +15,40 @@ using Plots, StatPlots, UnicodePlots
 # include functions
 include("load_data.jl")
 include("load_rdata.jl")
+include("save_data.jl")
 include("greenfield_stoch.jl")
 include("greenfield_determ.jl")
 
 # ------------------------------------------------------------------------------
 # load input data
 # ------------------------------------------------------------------------------
-sets, param = load_data("input_data", false, 80)
+folder      = "input_data"      # input data folder
+resShare    = 80                # RES share in %
+bisect      = true              # true = take every second hour
+h_stoch     = 1:24              # number of timesteps in stochastic model
+h_det       = 1:8760            # numer of timesteps in deterministic model
+
+sets, param = load_data(folder, bisect, resShare)
 
 # TODO: 2015 whole time series, all 5 years every second hour
 results  = Dict()
-results["Stochastic"] = invest_stochastic(sets, param, 1:24, GurobiSolver())
+results["Stochastic"] = invest_stochastic(sets, param, h_stoch, GurobiSolver())
 for year in sets["Years"]
     results["Deterministic"] = Dict()
     results["Deterministic"][year] = invest_deterministic(sets, param,
-        year, 1:8760, GurobiSolver())
+        year, h_det, GurobiSolver())
 end
+
+save_data(results, resShare, Dates.now())
 
 s = "Deterministic"
 # quick overview
 for z in sets["Zones"]
-    capacity = round.(Array(results[s]["1987"][z]["Capacity"])./1000,1)
-    capacity_type = names(results[s]["1987"][z]["Capacity"])[1]
-    storage_type = names(results[s]["1987"][z]["Storage Power"])[1]
-    storage = round.(Array(results[s]["1987"][z]["Storage Power"])./1000,1)
+    capacity = round.(Array(results[s]["2015"][z]["Capacity"])./1000,1)
+    capacity_type = names(results[s]["2015"][z]["Capacity"])[1]
+    storage_type = names(results[s]["2015"][z]["Storage Power"])[1]
+    storage = round.(Array(results[s]["2015"][z]["Storage Power"])./1000,1)
     label = vcat(capacity_type, storage_type)
     data = vcat(capacity, storage)
-    display(barplot(label, data, title=string(z, " installed capacity [MW]")))
+    display(barplot(label, data, title=string(z, " installed capacity [GW]")))
 end
-
-# Convert to exportable dataframe format
-x = convert(Array, results["Deterministic"]["1987"]["UK"]["Capacity"])
-df = DataFrame(x)
-names!(df, [Symbol(t) for t in sets["Tech"]])
-CSV.write("test", df)
