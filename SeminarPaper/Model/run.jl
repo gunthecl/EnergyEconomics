a638fd1d1c4f985a28aeef57f633b572f7a6e7d7
Unknown <chgentsch@googlemail.com>
2018-09-28 17:48:41 +0200

    Add Scenarios

--- SeminarPaper/Model/run.jl
+++ SeminarPaper/Model/run.jl
@@ -23,37 +23,37 @@ include("greenfield_determ.jl")
 # load input data
 # ------------------------------------------------------------------------------
 folder      = "input_data"      # input data folder
-resShare    = 80                # RES share in %
+resShare    = 95                # RES share in %
 bisect      = true              # true = take every second hour
-hSto        = 1:24              # number of timesteps in stochastic model
-hDet        = 1:4380            # numer of timesteps in deterministic model
+hoursSto    = 1:168             # number of timesteps in stochastic model
+hoursDet    = 1:4380            # numer of timesteps in deterministic model
 
 sets, param = load_data(folder, bisect, resShare)
 
 # TODO: 2015 whole time series, all 5 years every second hour
 results  = Dict()
-results["Stochastic"] = invest_stochastic(sets, param, h_stoch, GurobiSolver())
+results["Stochastic"] = invest_stochastic(sets, param, hoursSto, GurobiSolver())
 results["Deterministic"] = Dict()
 for year in sets["Years"]
     if year == "2016"
-
+        # Do nothing
     else
         results["Deterministic"][year] = invest_deterministic(sets, param,
-            year, h_det, GurobiSolver())
+            year, hoursDet, GurobiSolver())
     end
 end
 
-hours_det = string(length(hDet))
-hours_sto = string(length(hSto))
+hours_det = string(length(hoursDet))
+hours_sto = string(length(hoursSto))
 save_data(results, resShare, Dates.now(), hours_det, hours_sto)
 
-s = "Deterministic"
+s = "Stochastic"
 # quick overview
 for z in sets["Zones"]
-    capacity = round.(Array(results[s]["2015"][z]["Capacity"])./1000,1)
-    capacity_type = names(results[s]["2015"][z]["Capacity"])[1]
-    storage_type = names(results[s]["2015"][z]["Storage Power"])[1]
-    storage = round.(Array(results[s]["2015"][z]["Storage Power"])./1000,1)
+    capacity = round.(Array(results[s][z]["Capacity"])./1000,1)
+    capacity_type = names(results[s][z]["Capacity"])[1]
+    storage_type = names(results[s][z]["Storage Power"])[1]
+    storage = round.(Array(results[s][z]["Storage Power"])./1000,1)
     label = vcat(capacity_type, storage_type)
     data = vcat(capacity, storage)
     display(barplot(label, data, title=string(z, " installed capacity [GW]")))
