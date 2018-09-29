f47f13e3d9c3448102a1763f226529141d0e429e
Claudia <claudia-jena@gmx.de>
2018-09-29 23:57:04 +0200

    Added graphs

--- SeminarPaper/Model/output_data/2018-09-28 01_28_80%_8760h_Deterministic/Aggregate_years.R
+++ SeminarPaper/Model/output_data/2018-09-28 01_28_80%_8760h_Deterministic/Aggregate_years.R
@@ -19,8 +19,8 @@ org2015_aggr <- apply(dat.org2015, 2, sum)
 rep2015_aggr <- apply(dat.rep2015, 2, sum)
 
 
-#setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 06_12_40%_4380h_Deterministic")
-setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 20_15_80%_4380h_Deterministic")
+setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 06_12_40%_4380h_Deterministic")
+#setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 20_15_80%_4380h_Deterministic")
 
 wd <- getwd()
 
@@ -62,8 +62,7 @@ for (i in c("DE", "DK", "FR","IB", "LU", "UK")){
 
 }
 
-setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 06_12_40%_168h_Stochastic")
-
+setwd("/Users/claudiaguenther/Documents/EnergyEconomics/SeminarPaper/Model/output_data/2018-09-29 21_26_40%_168h_Stochastic")
 wd <- getwd()
 
 
